b916ea48fcf150800fe481b5e69d8f6279fa3529
Peter Botsford <petertoddbotsford@gmail.com>
2022-02-26 13:28:27 -0700

    added tests and made sure paths worked correctly

--- app.py
+++ app.py
@@ -28,26 +28,17 @@ def list_cupcakes():
 def get_cupcake(cupcake_id):
     """Gets information on specific cupcake"""
 
-    cupcake = Cupcake.get.query_or_404(cupcake_id)
-
-    cupcake.flavor = data['flavor']
-    cupcake.size = data['size']
-    cupcake.rating = data['rating']
-    cupcake.image = data['image']
-
-    db.session.add(cupcake)
-    db.session.commit()
-
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
     return jsonify(cupcake = cupcake.serialize())
 
-@app.route('/api/cupcakes', methods=['POST'])
+@app.route('/api/cupcakes', methods=["POST"])
 def new_cupcake():
     """adds a new cupcake to the database and displays it on the page"""
 
     data = request.json
 
     cupcake = Cupcake(
-        flavor = data['favor'],
+        flavor = data['flavor'],
         size = data['size'],
         rating = data['rating'],
         image = data['image'] or None
@@ -60,12 +51,16 @@ def new_cupcake():
 
 @app.route('/api/cupcake/<int:id>', methods=["PATCH"])
 def update_cupcake(id):
-    cupcake = cupcake.query.get_or_404(id)
-    cupcake.flavor = request.json.get('flavor', cupcake.flavor)
-    cupcake.size = request.json.get('size', cupcake.size)
-    cupcake.rating = request.json.get('rating', cupcake.rating)
-    cupcake.image = request.json.get('image', cupcake.image)
+    cupcake = Cupcake.query.get_or_404(id)
+    data = request.json
+    cupcake.flavor = data.get('flavor', cupcake.flavor)
+    cupcake.size = data.get('size', cupcake.size)
+    cupcake.rating = data.get('rating', cupcake.rating)
+    cupcake.image = data.get('image', cupcake.image)
+
+    db.session.add(cupcake)
     db.session.commit()
+
     return jsonify(cupcake=cupcake.serialize())
 
 @app.route('/api/cupcake/<int:id>', methods=["DELETE"])
